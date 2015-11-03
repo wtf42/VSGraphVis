a7c12a0773beae5a6ca59ad6b230429421784366
Evgeniy Akimov <geka666@gmail.com>
2015-11-03 23:18:45 +0300

    dump structure fields from VS debugger

---
+++ VSGraphViz/VSGraphVisualizerFactory.cs
@@ -0,0 +1,32 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace VSGraphVisualizer
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("code")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class VSGraphVisualizerFactory : IWpfTextViewCreationListener
+    {
+        private DTE2 m_dte;
+
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name(VSGraphVisualizer.AdornmentLayerName)]
+        [Order(After = PredefinedAdornmentLayers.Text)]
+        internal AdornmentLayerDefinition editorAdornmentLayer = null;
+
+        VSGraphVisualizerFactory()
+        {
+            m_dte = (DTE2)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+        }
+
+        public void TextViewCreated(IWpfTextView view)
+        {
+            view.Properties.GetOrCreateSingletonProperty<VSGraphVisualizer>(() => new VSGraphVisualizer(m_dte.Debugger, view));
+        }
+    }
+}
